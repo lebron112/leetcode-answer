8b85192ec323a500774d36d120d05c39f8200d00
lerbon112 <luohaibin@yagena.net>
2020-07-30 09:49:10 +0800

    formate file dir with number chunks

--- src/jobs.js
+++ src/jobs.js
@@ -1,13 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-const dir = process.cwd();
+const dir = __dirname;
 
-const list = fs.readdirSync(dir);
 /* 替换 1-xxx.js 成 1.xxx.js */
-list.forEach( async(item)=>{
-  const names  = item.split('.');
-  if(names[1] === 'js') {
-    const name = names[0].replace('-','.');
-    fs.renameSync(path.join(dir, item), path.join(dir, name + '.js'))
-  }
-});
+const formateName = () => {
+  const list = fs.readdirSync(__dirname);
+  list.forEach(async (item) => {
+    const names = item.split('.');
+    if (names[1] === 'js') {
+      const name = names[0].replace('-', '.');
+      fs.renameSync(path.join(__dirname, item), path.join(__dirname, name + '.js'))
+    }
+  });
+};
+// formateName();
+
+/** 把文件放入对应序号的文件夹 例如 1-50 51-100 101-150  */
+const pullFileIntoDir = () => {
+  const list = fs.readdirSync(__dirname);
+  list.forEach(item => {
+    const name = item.split('.');
+    if (!isNaN(name[0])) {
+      const num = Number(name[0]);
+      const res = Math.floor(num / 50);
+      let dirname = path.join(__dirname, `${res * 50 + 1}-${(res + 1) * 50}`);
+      const isExist = fs.existsSync(dirname);
+      if (!isExist) {
+        fs.mkdirSync(dirname)
+      }
+      fs.renameSync(path.join(__dirname, item), path.join(dirname, item));
+    }
+  });
+};
+pullFileIntoDir();
