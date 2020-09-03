59e4051437c34054bac1e73769be7660fc5469a1
lerbon112 <luohaibin@yagena.net>
2020-09-03 11:13:08 +0800

    num:31 total:106

--- src/1-50/31.下一个排列（未解决）.js
+++ src/1-50/31.下一个排列（未解决）.js
@@ -18,49 +18,65 @@
  */
 
 var nextPermutation = (nums) => {
-  if (!nums || !nums.length) return nums;
-  let i = nums.length;
-  let j;
-  let numVal
-  while (i > 0) {
-    for (j = 0; j < i - 1; j++) {
-      if (nums[j] < nums[j + 1]) {
-        [nums[j + 1], nums[j]] = [nums[j], nums[j + 1]];
+  // 基础情况处理
+  if (nums.length <= 2) return nums.reverse();
+  let extNumber = nums[nums.length - 1];
+  const last = [];
+  let hasChange = false;
+  // 2种情况判断
+  // 第一种情况 最后的数字比前一个数字大 直接交换位置即可
+  /* 第二种情况 更新最后一个值直到 后一个值比前一个值大，前一个值更新为后面数字里正好比他的的数，
+    然后把原来的丢入后面记录的数字里，后面的数字依次从小到大排序即可
+  */
+  for (let i = nums.length - 2; i >= 0;) {
+    const num = nums[i];
+    if (extNumber > num) {
+      hasChange = true;
+      if (!last.length) {
+        // 第一种情况
+        nums[i] = extNumber;
+        nums[i + 1] = num;
+      } else {
+        // 第二种情况
+        const arr = [...last, extNumber];
+        // 找出last里去掉比当前数字小的 ,所有比当前数字大的数字中最小的那个数字，替换掉当前位置
+        nums[i] = Math.min(...arr.filter(item => item > num));
+        // 把旧的数字丢路尾巴
+        arr.push(num);
+        // 筛选掉那个已被使用的数字 从该位置后面的都是从小到大的 
+        loopIn: for (let j = 0; j < arr.length; j++) {
+          if (arr[j] === nums[i]) {
+            arr.splice(j, 1); break loopIn;
+          }
+        }
+        nums.splice(i + 1, arr.length + 1, ...arr.sort((a, b) => a - b));
       }
-      numVal = Number([...nums].reverse().join(''));
-      console.log(numVal);
+      break;
+    } else {
+      last.push(extNumber);
+      extNumber = num;
+      i--;
     }
-    // return nums.reverse();
-    i--;
   }
-  if (i === 0) {
-    // nums.reverse();
-  }
+  // 循环结束 说明是正序的 直接反转即可
+  !hasChange && nums.reverse();
 };
-// var ar = [3, 2, 4, 9, 1, 5, 7, 6, 8];
-// // nextPermutation(ar);
-// console.log(ar);
+var ar = [7, 8, 9, 1, 2, 6, 5, 4, 3];
+nextPermutation(ar);
+console.log('res ar', ar);
 
 // let arr = [1, 3, 2, 4];
 // nextPermutation(arr);
-// console.log(arr);
+// console.log('res arr', arr);
 
-// let arr1 = [1, 2, 3];
-// nextPermutation(arr1);
-// console.log(arr1);
+let arr0 = [2, 3, 1];
+nextPermutation(arr0);
+console.log('res arr0', arr0);
 
-const arr_ = [1, 3, 2];
+let arr1 = [1, 3, 2];
+nextPermutation(arr1);
+console.log('res arr1', arr1);
+
+const arr_ = [3, 2, 1];
 nextPermutation(arr_);
-console.log(arr_);
-
-// const arr2 = [3, 2, 1];
-// nextPermutation(arr2);
-// console.log(arr2);
-
-// const arr3 = [1, 1, 5];
-// nextPermutation(arr3);
-// console.log(arr3);
-
-// const arr5 = [6, 7, 5, 3, 5, 6, 2, 9, 1, 2, 7, 0, 9];
-// nextPermutation(arr5);
-// console.log(arr5);
+console.log('res arr_', arr_);
