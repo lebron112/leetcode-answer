d5d9a028c8d35672b67e12caf0501e9797bd3e71
lerbon112 <luohaibin@yagena.net>
2020-09-15 18:40:23 +0800

    num:33 重做

--- src/1-50/33.搜索旋转排序数组.js
+++ src/1-50/33.搜索旋转排序数组.js
@@ -21,7 +21,39 @@
 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-// 暴力法 O(1)的复杂度
+// 暴力法 O(n)的复杂度
 const search = function (nums, target) {
   return nums.indexOf(target);
 };
+// 二分搜索
+const search2 = function (nums, target) {
+  const find = (start, end) => {
+    // console.log(start, end)
+    const mid = start + Math.floor((end - start) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[end] === target) return end;
+    if (nums[start] === target) return start;
+    if (start >= end) return -1;
+    if (target >= nums[start] && target <= nums[mid]) {
+      return find(start, mid - 1);
+    } else {
+      if (nums[start] > nums[mid] && nums[mid] > target || nums[end] < target && nums[mid] < nums[end]) {
+        return find(start, mid);
+      }
+      return find(mid + 1, end);
+    }
+  };
+  return find(0, nums.length - 1);
+}
+
+console.log(search2([2, 3, 4, 5, 6, 7, 8, 0, 1], 0));
+console.log(search2([4, 5, 6, 7, 0, 1, 2], 3));
+console.log(search2([5, 1, 3], 5));
+console.log(search2([5, 1, 3], 3));
+console.log(search2([1, 3], 3));
+console.log(search2([1, 2, 3, 4, 5, 6], 4));
+console.log(search2([5, 1, 2, 3, 4], 1));
+console.log(search2([5, 1, 2, 3, 4], 3));
+console.log(search2([4, 5, 1, 2, 3], 5));
+console.log(search2([4, 5, 6, 7, 8, 1, 2, 3], 8));
+console.log(search2([1], 5));
