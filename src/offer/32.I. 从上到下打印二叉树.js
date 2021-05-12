4fcba6dfbf1631a0bd886351ccb1db9d2d6409b6
lerbon112 <luohaibin@yagena.net>
2021-05-12 17:33:08 +0800

    total:152; num:offer-32-1

---
+++ src/offer/32.I. 从上到下打印二叉树.js
@@ -0,0 +1,52 @@
+/* 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+ 
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回：
+
+[3,9,20,15,7]
+ 
+
+提示：
+
+节点总数 <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 不能直接push 要和第二题一样处理
+ var levelOrder = function (root) {
+  const res = [];
+  const mapNode = (node, i) => {
+    if (node) {
+      const { val, left, right } = node;
+      if(!res[i]){
+        res[i] = []
+      }
+      res[i].push(val);
+      mapNode(left, i + 1);
+      mapNode(right, i + 1);
+    }
+  };
+  mapNode(root, 0);
+  return res.flat();
+};
