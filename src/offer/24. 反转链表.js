fd07a61024137ea5bf8dd4477f64dbe023a2eee1
lerbon112 <luohaibin@yagena.net>
2021-05-12 14:43:59 +0800

    num:offer24 以前做过

---
+++ src/offer/24. 反转链表.js
@@ -0,0 +1,44 @@
+/* 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+ 
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+ 
+
+限制：
+
+0 <= 节点个数 <= 5000
+
+ 
+
+注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
+// Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function (head) {
+  if (!head) return head;
+  let next = null;
+  while (head) {
+    let node = new ListNode(head.val);
+    node.next = next;
+    next = node;
+    head = head.next;
+  }
+  return next;
+};
