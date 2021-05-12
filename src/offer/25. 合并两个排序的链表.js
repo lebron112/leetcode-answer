c54ec285208c78819d7b2dd3fdf244f6916f30ae
lerbon112 <luohaibin@yagena.net>
2021-05-12 15:36:37 +0800

    num:offer-25 重新优化

---
+++ src/offer/25. 合并两个排序的链表.js
@@ -0,0 +1,53 @@
+/* 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+
+注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
+//  Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let res = null;
+  let next = null
+  const loop = (l1, l2) => {
+    if (l1 && l2) {
+      if (l1.val > l2.val) {
+        [l1, l2] = [l2, l1];
+      }
+    } else if (l1 || l2) {
+      l1 = l1 || l2;
+    }
+    if (l1) {
+      const node = new ListNode(l1.val);
+      if (!res) {
+        res = next = node;
+      } else {
+        next.next = node;
+        next = next.next;
+      }
+      l1 = l1.next;
+      loop(l1, l2);
+    }
+  };
+  loop(l1, l2);
+  return res;
+};
