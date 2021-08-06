c5be48e217e9a8fb7d2a7185ee30e02d141406f0
lerbon112 <luohaibin@yagena.net>
2021-08-06 16:31:41 +0800

    total:183; num:105

--- src/101-150/105. 从前序与中序遍历序列构造二叉树.js
+++ src/101-150/105. 从前序与中序遍历序列构造二叉树.js
@@ -26,19 +26,43 @@ inorder 保证为二叉树的中序遍历序列
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+}
+
 /**
  * @param {number[]} preorder
  * @param {number[]} inorder
  * @return {TreeNode}
  */
- var buildTree = function(preorder, inorder) {
-
+/*
+  递归法
+  从前序遍历取出第一个前序遍历的点，创建字数head
+  从中序遍历中找到这个头的索引位，左边为左子树a，右边为右子树
+  那么head的左子数，为下一次递归左子数的结果（下一次递归的前序遍历为去除右字数的结果，中序遍历为左子树）
+  右子数，为下一次递归右子数的结果（下一次递归的前序遍历为去除左字数的结果，中序遍历为右子树）
+ */
+var buildTree = function (preorder, inorder) {
+  if (!preorder.length) return null;
+  const root = preorder.shift();
+  let i = 0;
+  for (; i < inorder.length; i++) {
+    if (inorder[i] === root) {
+      break;
+    }
+  }
+  let left = inorder.slice(0, i);
+  let right = inorder.slice(i + 1);
+  let head = new TreeNode(root);
+  // 下一次递归的前序遍历为去除右字数的结果
+  let np = preorder.filter(item => left.includes(item));
+   // 下一次递归的前序遍历为去除左字数的结果
+  let nr =  preorder.filter(item => right.includes(item));
+  head.left = buildTree(np, left);
+  head.right = buildTree(nr, right);
+  return head;
 };
